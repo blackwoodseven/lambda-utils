4a14d63d9d36eb10c7aa92f032b58416771bc80e
Guillem Salas <gs@blackwoodseven.com>
2016-03-10 15:49:08 +0100

    Add support to store lambda configuration

--- src/latency/latency.js
+++ src/latency/latency.js
@@ -108,8 +108,47 @@ function run(lambda, options) {
   return deferred.promise();
 }
 
+function getStoreIndex() {
+  var existing = localStorage.getItem("lambdas");
+  if (!existing) {
+    existing = [];
+  } else {
+    existing = JSON.parse(existing);
+  }
+  return existing;
+}
+
+function storeOptions(ops) {
+  localStorage.setItem("lambda-"+ops.name, JSON.stringify(ops.data));
+
+  var existing = getStoreIndex();
+
+  existing.push(ops.name);
+  localStorage.setItem("lambdas", JSON.stringify(existing));
+}
+
+function recoverOptions() {
+  var index = getStoreIndex();
+
+  return index.map(function(name) {
+    return {
+      name: name,
+      data: JSON.parse(localStorage.getItem("lambda-"+name))
+    }
+  });
+}
+
+function clearOptions() {
+  var ops = getStoreIndex();
+  ops.forEach(function(name) {
+    localStorage.removeItem("lambda-"+name);
+  });
+  localStorage.removeItem("lambdas");
+}
+
 function test(lambda, iterations, delay) {
   document.getElementById("progress").innerHTML = "";
+  clearOptions();
   var lambdas = $(".lambda").map(function() {
     var $el = $(this),
         options = {
@@ -119,7 +158,7 @@ function test(lambda, iterations, delay) {
           data: $el.data("data")
         },
         promise = run(lambda, options);
-
+      storeOptions(options);
       var TPL = [
               '<hr/>',
               '<h5></h5>',
@@ -200,7 +239,8 @@ function showReport() {
   _.each(results, function(exec) {
     var success = _(exec.success).compact().pluck("time").value(),
         errors = _(exec.errors).compact().pluck("time").value(),
-        times = success.concat(errors);
+        times = success.concat(errors),
+        remoteTimes = _(exec.success).compact().pluck('data').pluck('time').value();
 
       str.push("\nFunction '"+exec.context.name + "' ( success: "+success.length+", errors: "+errors.length+")");
       str.push("----------------------------");
@@ -210,6 +250,7 @@ function showReport() {
 
       if (success.length)
       str.push("Success: "+analysis(success));
+      str.push("Remote:"+analysis(remoteTimes));
 
       if (errors.length)
       str.push("Errors: "+analysis(errors));
@@ -218,17 +259,24 @@ function showReport() {
         key: exec.context.name,
         values: _.pluck(exec.success, "time").map(function(v, i) {
           return {
-              y: v == null ? 0 : v,
+              y: v == null ? -1 : v,
               x: i
           }
         })
+      });
+
+      successTS.push({
+        key: 'Remote time for '+exec.context.name,
+        values: _(exec.success).pluck('data').pluck('time').map(function(v,i) {
+          return { y: v == null ? -1 : v, x: i }
+        }).value()
       });
 
       errorsTS.push({
         key: exec.context.name,
         values: _.pluck(exec.errors, "time").map(function(v, i) {
           return {
-              y: v == null ? 0 : v,
+              y: v == null ? -1 : v,
               x: i
           }
         })
@@ -279,6 +327,8 @@ document.forms.test.count.value = localStorage.getItem("exec-count") || 50;
 document.forms.test.aws_id.value = localStorage.getItem("aws-id") || "";
 document.forms.test.aws_secret.value = localStorage.getItem("aws-secret") || "";
 document.forms.test.aws_region.value = localStorage.getItem("aws-region") || 'eu-west-1';
+
+recoverOptions().forEach(addLambda);
 
 
 document.forms.test.onsubmit = function(ev) {
